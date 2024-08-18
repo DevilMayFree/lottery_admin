f515199c26d6437d4e98fbe707e9a7af3a41949d
fxdomcloud <fxdomcloud@gmail.com>
2024-08-18 18:17:02 +0800

    1

--- ruoyi-ui/src/views/lottery/goods/index.vue
+++ ruoyi-ui/src/views/lottery/goods/index.vue
@@ -254,7 +254,7 @@ export default {
         // 设置上传的请求头部
         headers: {Authorization: "Bearer " + getToken()},
         // 上传的地址
-        url: process.env.VUE_APP_BASE_API + "lottery/goods/uploadGoods"
+        url: process.env.VUE_APP_BASE_API + "/lottery/goods/uploadGoods"
       },
       // 查询参数
       queryParams: {
